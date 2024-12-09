45eb974c3cbd371f13809ed27361a6d0a1b679e4
Tatsh Siow <blussit@gmail.com>
2024-12-09 15:22:02 +0800

    .

--- 11063 - B2-Sequence.py
+++ 11063 - B2-Sequence.py
@@ -1,21 +1,20 @@
-count=1
+case=1
 while True:
   try:
     n=int(input())
     a=list(map(int,input().split()))
-    checker=0
+    check=0
     for i in range(1,n):
-      if int(a[i])==int(2*(a[i-1])):
-        checker=1
+      if int(2*(a[i-1]))==int(a[i]):
+        check=1
       else:
-        checker=0
-    if checker==1:
-      print (f"Case #{count}: It is a B2-Sequence.")
-      print()
-    if checker==0:
-      print (f"Case #{count}: It is not a B2-Sequence.")
-      print()
-    count+=1
+        check=0
+    if check==1:
+      print (f"Case #{case}: It is a B2-Sequence.")
+    if check==0:
+      print (f"Case #{case}: It is not a B2-Sequence.")
+    print()
+    case+=1
     input()
   except EOFError:
     break
