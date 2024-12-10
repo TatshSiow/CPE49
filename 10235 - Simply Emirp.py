05f430ea0278980931ea08c4c98da828c1b28997
Tatsh Siow <blussit@gmail.com>
2024-12-10 13:30:41 +0800

    .

--- 10235 - Simply Emirp.py
+++ 10235 - Simply Emirp.py
@@ -1,11 +1,12 @@
+import math
 def is_prime(n):
-    return n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))
+    return n > 1 and all(n % i for i in range(2, int(math.sqrt(n)) + 1))
 
 while True:
     try:
         n = int(input())
-        n_s = str(n)[::-1]
-        if is_prime(n) and int(n_s) != n and is_prime(int(n_s)):
+        n_s = int(str(n)[::-1])
+        if is_prime(n) and n_s != n and is_prime(n_s):
             print(f"{n} is emirp.")
         elif is_prime(n):
             print(f"{n} is prime.")
